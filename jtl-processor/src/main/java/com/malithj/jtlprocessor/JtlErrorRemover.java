d351cb012d5c0b6d8eb919443c20ce389ced0fa6
Malith Jayasinghe <malith.jayasinghe@gmail.com>
2017-10-03 16:59:22 +0530

    jtl processor

---
+++ jtl-processor/src/main/java/com/malithj/jtlprocessor/JtlErrorRemover.java
@@ -0,0 +1,28 @@
+package com.malithj.jtlprocessor;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.BufferedWriter;
+import java.io.BufferedReader;
+import java.io.FileWriter;
+
+/**
+ * Created by temp on 10/2/17.
+ */
+public class JtlErrorRemover {
+    public static void main( String[] args )
+    {
+        File fileToProcess = new File(args[1]);
+        try (BufferedReader fileReader = new BufferedReader(new FileReader(fileToProcess))) {
+            try (BufferedWriter fileWriter = new BufferedWriter(new FileWriter(fileToProcess+"_"+"processed"))) {
+                String line = fileReader.readLine();
+                if (line != null) {
+                    fileWriter.write(line);
+                    fileWriter.newLine();
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
