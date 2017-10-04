06a7747d306df6f7e091ff1dfc0f7162d8979646
Malith Jayasinghe <malith.jayasinghe@gmail.com>
2017-10-04 09:54:38 +0530

    contains has better performance compared to regex

--- jtl-processor/src/main/java/com/malithj/jtlprocessor/JtlErrorRemover.java
+++ jtl-processor/src/main/java/com/malithj/jtlprocessor/JtlErrorRemover.java
@@ -15,27 +15,17 @@ public class JtlErrorRemover {
     public static void main( String[] args )
     {
         File fileToProcess = new File(args[0]);
-
-        String successRegex = "(.*?),200,OK,(.*?)";
-
-
-        Pattern   compiledPattern = Pattern.compile(successRegex);
-
-
-        //File fileToProcess = new File("/Users/temp/jtl-error-remover/jtl-processor/C_500_T_600_P_100K_buyStocks.xml.jtl");
-
         try (BufferedReader fileReader = new BufferedReader(new FileReader(fileToProcess))) {
             try (BufferedWriter fileWriter = new BufferedWriter(new FileWriter(fileToProcess+"_"+"processed"))) {
                 String line;
                 while((line = fileReader.readLine()) != null) {
-                    Matcher m1 = compiledPattern.matcher(line);
-                    if (m1.find()) {
-                        System.out.print(line + "\n");
+                    if(line.contains("OK")) {
                         fileWriter.write(line);
                         fileWriter.newLine();
                     }
                 }
             }
+            System.out.println("done");
         } catch (Exception e) {
             e.printStackTrace();
         }
