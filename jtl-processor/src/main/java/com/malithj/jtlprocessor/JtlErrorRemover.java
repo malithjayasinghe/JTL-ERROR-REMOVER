9614555726457367d28b8d0952b8ca0a3eff414f
Malith Jayasinghe <malith.jayasinghe@gmail.com>
2017-10-04 09:28:55 +0530

    adding pattern matching

--- jtl-processor/src/main/java/com/malithj/jtlprocessor/JtlErrorRemover.java
+++ jtl-processor/src/main/java/com/malithj/jtlprocessor/JtlErrorRemover.java
@@ -5,6 +5,8 @@ import java.io.FileReader;
 import java.io.BufferedWriter;
 import java.io.BufferedReader;
 import java.io.FileWriter;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Created by temp on 10/2/17.
@@ -12,13 +14,26 @@ import java.io.FileWriter;
 public class JtlErrorRemover {
     public static void main( String[] args )
     {
-        File fileToProcess = new File(args[1]);
+        //File fileToProcess = new File(args[0]);
+
+        String successRegex = "(.*?),200,OK,(.*?)";
+
+
+        Pattern   compiledPattern = Pattern.compile(successRegex);
+
+
+        File fileToProcess = new File("/Users/temp/jtl-error-remover/jtl-processor/C_500_T_600_P_100K_buyStocks.xml.jtl");
+
         try (BufferedReader fileReader = new BufferedReader(new FileReader(fileToProcess))) {
             try (BufferedWriter fileWriter = new BufferedWriter(new FileWriter(fileToProcess+"_"+"processed"))) {
-                String line = fileReader.readLine();
-                while(line != null) {
-                    fileWriter.write(line);
-                    fileWriter.newLine();
+                String line;
+                while((line = fileReader.readLine()) != null) {
+                    Matcher m1 = compiledPattern.matcher(line);
+                    if (m1.find()) {
+                        System.out.print(line + "\n");
+                        fileWriter.write(line);
+                        fileWriter.newLine();
+                    }
                 }
             }
         } catch (Exception e) {
